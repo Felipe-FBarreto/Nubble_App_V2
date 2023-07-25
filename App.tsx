891e992bcb0a10cad335e8b1a521aa2e4b9d8cf2
Felipe Barreto <felipebarreto1050@gmail.com>
2023-07-25 15:02:21 -0300

    feat:create theme colors global

--- App.tsx
+++ App.tsx
@@ -2,15 +2,19 @@ import {View, SafeAreaView} from 'react-native';
 import React from 'react';
 import Text from './src/componets/Text/Text';
 import Button from './src/componets/Button/Button';
+import {ThemeProvider} from '@shopify/restyle';
+import {theme} from './src/theme/theme';
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <View style={{paddingHorizontal: 20}}>
-        <Text bold>Hello Word</Text>
-        <Button />
-      </View>
-    </SafeAreaView>
+    <ThemeProvider theme={theme}>
+      <SafeAreaView>
+        <View style={{paddingHorizontal: 20}}>
+          <Text bold>Hello Word</Text>
+          <Button />
+        </View>
+      </SafeAreaView>
+    </ThemeProvider>
   );
 };
 
--- src/componets/Button/Button.tsx
+++ src/componets/Button/Button.tsx
@@ -1,19 +1,21 @@
 import {TouchableOpacity} from 'react-native';
 import React from 'react';
 import Text from '../Text/Text';
+import {useAppTheme} from '../../hooks/useAppTheme';
 
 export default function Button() {
+  const {colors} = useAppTheme();
   return (
     <TouchableOpacity
       style={{
         width: '100%',
         height: 40,
-        backgroundColor: '#550ab1',
+        backgroundColor: colors.primary,
         justifyContent: 'center',
         alignItems: 'center',
         borderRadius: 12,
       }}>
-      <Text bold preset="headingMedium" style={{color: '#fff'}}>
+      <Text bold preset="headingMedium" style={{color: colors.background}}>
         CoffStack
       </Text>
     </TouchableOpacity>
