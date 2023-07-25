04827daa097285117d50945058e774c2edf5ae39
Felipe Barreto <felipebarreto1050@gmail.com>
2023-07-24 21:07:50 -0300

    Firt Commit Feat: create Text Global

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import {View, SafeAreaView} from 'react-native';
+import React from 'react';
+import Text from './src/componets/Text/Text';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hello Word</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
